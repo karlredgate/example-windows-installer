5bd4ec7290b3a686a6346ff8f91dea298ec41f35
Karl Redgate <Karl.Redgate@gmail.com>
2015-03-03 17:22:32 -0500

    add PS profile

---
+++ PowerShell/Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,8 @@
+<#
+ .Description
+ This is the default profile for Administrator on a recovery
+ host.  It loads the Example module so that the WinRM calls from
+ the API host see these commands as "built in"
+#>
+
+Import-Module Example
